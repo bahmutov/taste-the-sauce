56fea3fd3603583176081ddf12eb802a16a69b31
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-31 16:07:18 -0500

    intro

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,34 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+it('visits the store', () => {
+  cy.visit('/')
+  // Tip: grab the username and the password from the login page
+  // It is ok for now to hardcode it in the spec source here
+  //
+  // get the username field and type the standard user
+  // https://on.cypress.io/get
+  // https://on.cypress.io/type
+  cy.get('[data-test="username"]').type('standard_user')
+  // get the password field and type the password
+  cy.get('[data-test="password"]').type('secret_sauce')
+  // get the login button and click on it
+  // https://on.cypress.io/click
+  cy.get('[data-test="login-button"]').click()
+  // you should transition to the inventory page
+  // https://on.cypress.io/location
+  // see assertion examples at
+  // https://glebbahmutov.com/cypress-examples/commands/location.html
+  cy.location('pathname').should('equal', '/inventory.html')
+  // confirm the inventory page really loads
+  // check that the inventory list is visible
+  // and there are at least 3 items
+  // https://on.cypress.io/get
+  // https://on.cypress.io/find
+  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+  cy.get('.inventory_list')
+    .should('be.visible')
+    .find('.inventory_item')
+    .should('have.length.greaterThan', 3)
+})
