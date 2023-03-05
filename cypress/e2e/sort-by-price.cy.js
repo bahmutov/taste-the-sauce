e33ddaf3e1639a845e2f74a77d70bcf798fe22b3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-05 14:10:39 -0500

    set the initial spec

---
+++ cypress/e2e/sort-by-price.cy.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+// https://github.com/bahmutov/cypress-map
+import 'cypress-map'
+
+// https://www.chaijs.com/plugins/chai-sorted/
+chai.use(require('chai-sorted'))
+
+describe('sorting', () => {
+  it('by price lowest to highest', () => {
+    cy.log('**log in**')
+    cy.visit('/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.location('pathname').should('equal', '/inventory.html')
+    cy.log('**sort by price low to high**')
+    // sort the items from low to high
+    // confirm the item prices are sorted in ascending order
+  })
+
+  it('by price highest to highest', () => {
+    cy.log('**log in**')
+    cy.visit('/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.location('pathname').should('equal', '/inventory.html')
+    cy.log('**sort by price low to high**')
+    // sort the items from high to low price
+    // confirm the item prices are sorted from highest to lowest
+  })
+})
