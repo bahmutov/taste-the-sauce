9cb5f01f939fc7c7db920ff7bf602ffff5be9d39
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-05 15:47:13 -0500

    add fixtures folder

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     // baseUrl, etc
     baseUrl: 'http://localhost:3000',
     supportFile: false,
-    fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // and load any plugins that require the Node environment
