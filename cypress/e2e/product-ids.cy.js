3b552fe94fa1204aa9936a184c0481d878d77472
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-12 11:17:53 -0400

    add spec

---
+++ cypress/e2e/product-ids.cy.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+import { LoginPage } from './login.page'
+
+describe('Products', () => {
+  // create a small type on the fly using jsdoc comment
+  // just to help type check help us
+  /** @type {{username: string, password: string}} */
+  const user = Cypress.env('users').standard
+  // we can even check if the user object is valid
+  if (!user) {
+    throw new Error('Missing the standard user')
+  }
+
+  // before each test, quickly login the user
+  // or restore the previous user session
+  beforeEach(() => {
+    LoginPage.login(user.username, user.password)
+    cy.visit('/inventory.html')
+    cy.location('pathname').should('equal', '/inventory.html')
+  })
+
+  it('have unique ids', () => {
+    // get all inventory items, there should be more than 3
+    // https://on.cypress.io/get
+    // https://on.cypress.io/should
+    //
+    // from each element, get the attribute "data-itemid"
+    // and confirm the ids are unique
+    // https://on.cypress.io/invoke
+    // https://glebbahmutov.com/cypress-examples
+  })
+})
