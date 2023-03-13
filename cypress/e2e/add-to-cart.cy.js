fc290987155eefaebde9c3b2921d5b248562fdcd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-12 20:44:10 -0400

    initial spec for add to cart refactoring

--- cypress/e2e/add-to-cart.cy.js
+++ cypress/e2e/add-to-cart.cy.js
@@ -24,27 +24,43 @@ describe('Cart', () => {
     cy.location('pathname').should('equal', '/inventory.html')
   })
 
-  it('adds items to the cart', () => {
+  it('adds items to the cart', { viewportHeight: 1200 }, () => {
     // confirm the cart badge does not exist at first
-    //
+    cy.get('.shopping_cart_link')
+      .find('.shopping_cart_badge')
+      .should('not.exist')
+
     // add the item "Sauce Labs Bike Light" to the cart
     // by clicking the button "Add to cart"
-    // the button should switch to "Remove"
-    //
+    cy.contains('.inventory_item', 'Sauce Labs Bike Light').within(() => {
+      cy.contains('button', 'Add to cart').click()
+      // the button should switch to "Remove"
+      cy.contains('button', 'Add to cart').should('not.exist')
+      cy.contains('button', 'Remove')
+    })
     // the shopping cart link should have number 1
-    // tip: use https://on.cypress.io/scrollintoview command
-    // to bring the shopping cart element into the viewport
-    //
+    cy.get('.shopping_cart_link')
+      .contains('.shopping_cart_badge', 1)
+      .scrollIntoView()
+      .should('be.visible')
     // add the item "Sauce Labs Bolt T-Shirt" to the cart
     // by clicking the button "Add to cart"
-    // the button should switch to "Remove"
-    //
+    cy.contains('.inventory_item', 'Sauce Labs Bolt T-Shirt').within(() => {
+      cy.contains('button', 'Add to cart').click()
+      // the button should switch to "Remove"
+      cy.contains('button', 'Add to cart').should('not.exist')
+      cy.contains('button', 'Remove')
+    })
     // the shopping cart link should have number 2
     // tip: use https://on.cypress.io/scrollintoview command
     // to bring the shopping cart element into the viewport
-    //
+    cy.get('.shopping_cart_link')
+      .contains('.shopping_cart_badge', 2)
+      .scrollIntoView()
+      .should('be.visible')
     // there should be two buttons with text "Remove"
     // on the inventory page
     // https://glebbahmutov.com/cypress-examples/#querying
+    cy.get('.inventory_item:contains("Remove")').should('have.length', 2)
   })
 })
