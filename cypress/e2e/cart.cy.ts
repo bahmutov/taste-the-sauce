a59937fe0b92f042bdceb824765d27eae4560a9d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-15 07:31:31 -0400

    cart items spec

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -27,22 +27,46 @@ describe('Cart', () => {
         'Sauce Labs Onesie',
       ]
       // add each item to cart using the InventoryPage object
+      items.forEach(InventoryPage.addItemToCart)
       cy.log('**added all items to cart**')
       // confirm the cart badge shows the right number of items
       // then click on it
       // https://on.cypress.io/click
+      InventoryPage.getCartBadge()
+        .should('have.text', items.length)
+        .scrollIntoView()
+        .wait(1000)
+        .click()
       // confirm we move to the cart page
       // https://on.cypress.io/location
-      //
+      cy.location('pathname').should('equal', '/cart.html')
       // confirm the cart items list has the right number of elements
+      cy.get('.cart_list .cart_item').should('have.length', items.length)
       cy.log('**shows each item in order**')
       // iterate over the items
-      // confirm each itm is at the right place
-      // on the page in the list of items
-      // https://on.cypress.io/get
-      // https://on.cypress.io/eq
-      // and confirm that within the item the name
-      // is correct and the quantity is 1
+      items.forEach((itemName, k) => {
+        // confirm each itm is at the right place
+        // on the page in the list of items
+        // https://on.cypress.io/get
+        // https://on.cypress.io/eq
+        cy.get('.cart_list .cart_item')
+          .eq(k)
+          .within(() => {
+            // and confirm that within the item the name
+            // is correct and the quantity is 1
+            cy.contains('.inventory_item_name', itemName)
+            cy.contains('.cart_quantity', 1)
+          })
+      })
+      // get the application window object
+      // https://on.cypress.io/window
+      // get its property "localStorage"
+      // https://on.cypress.io/its
+      // and call the method "getItem" to get the cart contents
+      // https://on.cypress.io/invoke
+      // confirm the list is [0, 1, 2]
+      // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+      // Tip: local storage usually has stringified JSON
     },
   )
 })
