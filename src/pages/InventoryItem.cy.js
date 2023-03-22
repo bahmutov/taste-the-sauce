8f54a5c5d1711bafdd4bb557a8c3b8fa81b835d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-22 14:00:24 -0400

    lesson 59

---
+++ src/pages/InventoryItem.cy.js
@@ -0,0 +1,21 @@
+import InventoryItem from './InventoryItem'
+import { InventoryData } from '../utils/InventoryData'
+
+describe('InventoryItem', () => {
+  it('shows an item', () => {
+    // what do you see when you try to mount the component?
+    // cy.mount(<InventoryItem />)
+    // what do you see when you mount the component by itself?
+    // cy.mountWithRouter(<InventoryItem />)
+    //
+    // pick a random number between 0 and the inventory length
+    // using Lodash Cypress._.random method
+    //
+    // mount the InventoryItem component
+    // passing your own search string "id=..."
+    // Tip: modify the InventoryItem to read the window.location.search
+    // or the passed search prop
+    //
+    // confirm the component shows the item's name, description, and price
+  })
+})
