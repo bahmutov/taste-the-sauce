b6633c9d5de707bbf5dc0cba7d8e3e16de2619d8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-22 09:43:35 -0400

    button click spec

--- src/components/Button.cy.js
+++ src/components/Button.cy.js
@@ -4,3 +4,21 @@ it('shows a button', () => {
   cy.mount(<Button label="Test button" />)
   cy.contains('button', 'Test button')
 })
+
+it('calls the click prop', () => {
+  // mount the Button component
+  // with label "Test button" and "onClick" prop
+  // The "onClick" prop should be a Cypress Sinon stub function
+  // with an alias "click"
+  // https://on.cypress.io/stub
+  // https://on.cypress.io/as
+  //
+  // query the page to find the button with text "Test button"
+  // and click on it
+  // https://on.cypress.io/contains
+  // https://on.cypress.io/click
+  //
+  // get the "click" stub and confirm it was clicked once
+  // https://on.cypress.io/get
+  // https://glebbahmutov.com/cypress-examples/commands/spies-stubs-clocks.html
+})
