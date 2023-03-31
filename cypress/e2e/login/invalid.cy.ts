d3d2ee4c5af527b68103df33708eef07c31b91cb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-31 07:27:23 -0400

    Revert "test the invalid user"
    This reverts commit 929a6744fd09424046fd34740edbf73d4d644e62.

--- cypress/e2e/login/invalid.cy.ts
+++ cypress/e2e/login/invalid.cy.ts
@@ -8,30 +8,20 @@ describe('Invalid user information', () => {
   it('wrong password', () => {
     cy.visit('/')
     // type the valid username and some made up password
-    LoginPage.getUsername().type(user.username)
-    LoginPage.getPassword().type('incorrect-password')
+    //
     // click on the login button
     // https://on.cypress.io/click
-    cy.getByTest('login-button').click()
+    //
     // confirm the page shows errors and stays on login URL
-    LoginPage.showsError(
-      'Epic sadface: Username and password do not match any user in this service',
-    )
-    cy.location('pathname').should('equal', '/')
   })
 
   it('wrong username and password', () => {
     cy.visit('/')
     // make up both username and password
-    LoginPage.getUsername().type('user-not-found')
-    LoginPage.getPassword().type('incorrect-password')
+    //
     // click on the login button
     // https://on.cypress.io/click
-    cy.getByTest('login-button').click()
+    //
     // confirm the page shows errors and stays on login URL
-    LoginPage.showsError(
-      'Epic sadface: Username and password do not match any user in this service',
-    )
-    cy.location('pathname').should('equal', '/')
   })
 })
