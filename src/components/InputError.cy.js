0d1161833de5edf4305cfea081993d234e80afc7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-31 09:26:24 -0400

    add input error

---
+++ src/components/InputError.cy.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import InputError, { INPUT_TYPES } from './InputError'
+
+it('uses test id for several attributes', () => {
+  cy.mount(
+    <InputError
+      isError={false}
+      type={INPUT_TYPES.TEXT}
+      value="Hello"
+      testId="greeting"
+    />,
+  )
+  // confirm the input element has id, name, and data-test attributes
+  // all having value "greeting"
+})
+
+it('does not set default test id', () => {
+  cy.mount(<InputError isError={false} type={INPUT_TYPES.TEXT} value="Hello" />)
+  // confirm the input element has no id, no name, and no data-test attributes
+  // tip: how many times do you need to query the page?
+})
