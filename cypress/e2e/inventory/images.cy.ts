463fa1df1d0c31243ab4a82ffc0b5084cd9cfd5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-01 10:18:35 -0400

    bonus 28

--- cypress/e2e/inventory/images.cy.ts
+++ cypress/e2e/inventory/images.cy.ts
@@ -3,6 +3,12 @@ import { LoginPage } from '@support/pages/login.page'
 
 describe('Regular user', { viewportHeight: 1200 }, () => {
   beforeEach(() => {
+    // uncomment to simulate an image load failure
+    // cy.intercept(
+    //   { resourceType: 'image', pathname: /\/bolt-shirt/ },
+    //   { statusCode: 404 },
+    // )
+
     const user: LoginInfo = Cypress.env('users').standard
     LoginPage.login(user.username, user.password)
     cy.visit('/inventory.html')
@@ -34,5 +40,17 @@ describe('Regular user', { viewportHeight: 1200 }, () => {
     // https://on.cypress.io/each
     // confirm the image element has loaded using its naturalWidth property
     // Tip: include the alt text in the assertion message
+    cy.get('img').each(($image: JQuery<HTMLImageElement>) => {
+      // $image is a jQuery object with a single IMG element inside
+      const src = $image[0].src
+      // src could be data images or pretty long URL
+      // let's skip the middle part
+      const short = src.slice(0, 20) + '...' + src.slice(src.length - 20)
+      const alt = $image[0].alt
+      expect(
+        $image[0].naturalWidth,
+        `natural width for "${alt}"`,
+      ).to.be.greaterThan(0)
+    })
   })
 })
