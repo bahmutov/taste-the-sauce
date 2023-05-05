d9da7262b72484f89f26dba4dc498755ede03608
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-05 17:17:04 +0300

    Add test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.title').should('text', 'Products')
   })
 })
