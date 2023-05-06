1611effc9bc30cfc01c651a4601906af6de11cbc
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-06 16:25:23 +0300

    cypress-mochawesome-reporter 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,20 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      allureWriter(on, config);
+      return config;
       // implement node event listeners here
     },
   },
-};
+});
