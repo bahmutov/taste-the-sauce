b8202925c59656620680685394b766726f5677d0
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-06 16:22:09 +0300

    Revert "cypress-mochawesome-reporter"
    This reverts commit 59926b031394b2815798699175e29a151f3e79d1.

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      allureWriter(on, config);
-      return config;
       // implement node event listeners here
     },
   },
-}
+};
