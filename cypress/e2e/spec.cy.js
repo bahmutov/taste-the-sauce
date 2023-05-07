f8215d14e77c1e5a26787758de41b4309dd7e74d
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-07 19:06:58 +0300

    Test1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,6 @@ describe('template spec', () => {
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
-    cy.get('.title').should('text', 'Products1')
+    cy.get('.title').should('text', 'Products')
   })
 })
