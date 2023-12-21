4e4085160053f206d7d190f870603af3042fb45f
emmabubu <100118606+emmabubu@users.noreply.github.com>
2023-12-21 15:16:32 +0800

    my lesson 2 solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,32 +1,25 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
+import 'cypress-map'
+const { _ } = Cypress
 
-it('visit the page', () => {
+it('confirms the item with the lowest price', () => {
   cy.visit('/')
-  // Tip: grab the username and the password from the login page
-  // It is ok for now to hardcode it in the spec source here
-  //
-  // get the username field and type the standard user
-  // https://on.cypress.io/get
-  // https://on.cypress.io/type
+
   cy.get('[data-test="username"]').type('standard_user')
-  // get the password field and type the password
   cy.get('[data-test="password"]').type('secret_sauce')
-  // get the login button and click on it
-  // https://on.cypress.io/click
   cy.get('[data-test="login-button"]').click()
-  // you should transition to the inventory page
-  // https://on.cypress.io/location
-  // see assertion examples at
-  // https://glebbahmutov.com/cypress-examples/commands/location.html
-  //
-  // confirm the inventory page really loads
   cy.location('pathname').should('equal', '/inventory.html')
-  // check that the inventory list is visible
-  // and there are at least 3 items
-  // https://on.cypress.io/get
-  // https://on.cypress.io/find
-  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
   cy.get('.inventory_item').should('have.length.gte', 3)
+
+  cy.get('.inventory_list')
+    .find('.inventory_item_price')
+    .map('innerText')
+    .print()
+    .mapInvoke('substring', 1)
+    .map(parseFloat)
+    .print()
+    .apply(_.min)
+    .should('equal', 7.99)
 })
